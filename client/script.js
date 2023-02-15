ed03405c1b824f92bbdf6a58f91dbfafa5c68d4a
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2023-02-15 15:55:26 +0530

    second commit

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
-  const response = await fetch("https://mahagpt.onrender.com/", {
+  const response = await fetch("https://mahagpt.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
